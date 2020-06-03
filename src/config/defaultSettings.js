42e5d148be5e798009ced4f8054be5e6d255690b
faithcoco <i13166380861@163.com>
2020-06-03 20:42:05 +0800

    修改默认配置

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -13,8 +13,9 @@
  *
  */
 
+
 export default {
-  primaryColor: '#52C41A', // primary color of ant design
+  primaryColor: '#1890FF', // primary color of ant design
   navTheme: 'dark', // theme for nav menu
   layout: 'sidemenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fixed', // layout of content: Fluid or Fixed, only works when layout is topmenu
@@ -26,8 +27,8 @@ export default {
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
   // vue-ls options
   storageOptions: {
-    namespace: 'pro__', // key prefix
-    name: 'ls', // name variable Vue.[ls] or this.[$ls],
-    storage: 'local' // storage name session, local, memory
+    namespace: 'pro__',
+    name: 'ls',
+    storage: 'local',
   }
 }

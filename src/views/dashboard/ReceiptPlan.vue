d30fe60a83224f82a973d85693451718c75d22f8
wuyang0425 <66615157+wuyang0425@users.noreply.github.com>
2020-06-17 11:22:34 +0800

    仪表盘开发

---
+++ src/views/dashboard/ReceiptPlan.vue
@@ -0,0 +1,99 @@
+<template>
+  <a-table
+    :columns="columns"
+    :data-source="data"
+    bordered
+    size="middle"
+    :scroll="{ x: 'calc(400px + 50%)', y: 400 }"
+  />
+</template>
+<script>
+const columns = [
+  {
+    title: '收货日期',
+    dataIndex: 'ReceiptDate',
+    key: 'ReceiptDate',
+    width: 10
+  },
+  {
+    title: '供应商名称',
+    dataIndex: 'SupplierName',
+    key: 'SupplierName',
+    width: 10
+  },
+  {
+    title: '超期多少天',
+    dataIndex: 'Overdue',
+    key: 'Overdue',
+    width: 10
+  },
+  {
+    title: '产品编码',
+    dataIndex: 'ProductCode',
+    key: 'ProductCode',
+    width: 10
+  },
+  {
+    title: '产品名称',
+    dataIndex: 'ProductName',
+    key: 'ProductName',
+    width: 10
+  },
+  {
+    title: '发货仓库',
+    dataIndex: 'ShippingWarehouse',
+    key: 'ShippingWarehouse',
+    width: 10
+  },
+  {
+    title: '发货数量',
+    dataIndex: 'ShippingQuantity',
+    key: 'ShippingQuantity',
+    width: 10
+  },
+  {
+    title: '计量单位',
+    dataIndex: 'Unit',
+    key: 'Unit',
+    width: 10
+  },
+  {
+    title: '包装数量',
+    dataIndex: 'PackingQuantity',
+    key: 'PackingQuantity',
+    width: 10
+  },
+  {
+    title: '包装单位',
+    dataIndex: 'PackingUnit',
+    key: 'PackingUnit',
+    width: 10
+  }
+]
+
+const data = []
+for (let i = 1; i < 30; i++) {
+  data.push({
+    key: i,
+    ReceiptDate: '1.' + i,
+    SupplierName: `Edward King ${i}`,
+    Overdue: i,
+    ProductCode: 'A111' + i,
+    ProductName: `Edward Name ${i}`,
+    ShippingWarehouse: 100 + i,
+    ShippingQuantity: 1000 + i,
+    Unit: '',
+    PackingQuantity: '',
+    PackingUnit: ''
+  })
+}
+
+export default {
+  data() {
+    return {
+      data,
+      columns
+    }
+  }
+}
+</script>

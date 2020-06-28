ba01027d9ff6e290403519d7e791e6e03b11ca88
wuyang0425 <66615157+wuyang0425@users.noreply.github.com>
2020-06-28 17:19:23 +0800

    完成新功能

--- src/views/management/Inventory/OpeningInventoryList.vue
+++ src/views/management/Inventory/OpeningInventoryList.vue
@@ -17,25 +17,26 @@
         </a-col>
       </a-row>
       <br />
-      <a-table
-        :row-selection="rowSelection"
+      <s-table
+        ref="table"
+        size="default"
         :columns="targetTitle"
-        :data-source="data"
+        :data="loadData"
+        :alert="false"
+        :scroll="{ x: 1500 }"
         bordered
-        size="middle"
-        :scroll="{ x: 'calc(400px + 50%)', y: 400 }"
       >
-        <a slot="name" slot-scope="text, record" @click="handleSearch(record)">{{ text }}</a>
+        <a slot="name" slot-scope="text, record" @click="handleSearch(record)">{{ text }}</a>
+
         <span slot="action" slot-scope="text, record">
-          <template>
+          <template v-if="$auth('table.update')">
             <a @click="handleEdit(record)">编辑</a>
             <a-divider type="vertical" />
-            <a-popconfirm v-if="data.length" title="确定要删除吗?" @confirm="() => onDelete(record.key)">
-              <a href="javascript:;">删除</a>
-            </a-popconfirm>
+            <a @click="handleEdit(record)">删除</a>
           </template>
         </span>
-      </a-table>
+        6t
+      </s-table>
     </a-card>
     <a-drawer
       title="产品详情"
@@ -61,6 +62,7 @@
           label="Address"
         >No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China</a-descriptions-item>
       </a-descriptions>
+      <a-button type="primary" @click="chatClick">聊一聊</a-button>
     </a-drawer>
     <a-modal
       title="Title"
@@ -81,21 +83,73 @@
         @scroll="handleScroll"
       />
     </a-modal>
+    <a-modal
+      width="1000px"
+      title="评论"
+      :visible="chat_visible"
+      :confirm-loading="confirmLoading"
+      @ok="chatOk"
+      @cancel="chatCancel"
+    >
+      <div>
+        <a-list
+          v-if="comments.length"
+          :data-source="comments"
+          :header="`${comments.length} ${comments.length > 1 ? '回复' : '回复'}`"
+          item-layout="horizontal"
+        >
+          <a-list-item slot="renderItem" slot-scope="item">
+            <a-comment
+              :author="item.author"
+              :avatar="item.avatar"
+              :content="item.content"
+              :datetime="item.datetime"
+            />
+          </a-list-item>
+        </a-list>
+        <a-comment>
+          <a-avatar
+            slot="avatar"
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            alt="Han Solo"
+          />
+          <div slot="content">
+            <a-form-item>
+              <a-textarea :rows="4" :value="value" @change="chatChange" />
+            </a-form-item>
+            <a-form-item>
+              <a-button
+                html-type="submit"
+                :loading="submitting"
+                type="primary"
+                @click="handleSubmit"
+              >评论</a-button>
+            </a-form-item>
+          </div>
+        </a-comment>
+      </div>
+    </a-modal>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 import Vue from 'vue'
 import { Descriptions } from 'ant-design-vue'
 import { Transfer } from 'ant-design-vue'
+import { Comment } from 'ant-design-vue'
 Vue.use(Descriptions)
 Vue.use(Transfer)
+Vue.use(Comment)
+import STree from '@/components/Tree/Tree'
+import { STable } from '@/components'
+import { getOrgTree, getServiceList } from '@/api/manage'
 
 const columns = [
   {
     key: '0',
     title: '存货编码',
-    dataIndex: 'name',
+    dataIndex: 'Type',
     defaultSortOrder: 'descend',
     sorter: (a, b) => a.age - b.age,
     scopedSlots: { customRender: 'name' }
@@ -103,7 +157,7 @@ const columns = [
   {
     key: '1',
     title: '仓库编码',
-    dataIndex: 'code',
+    dataIndex: 'StorageProduct',
     defaultSortOrder: 'descend',
     sorter: (a, b) => a.name - b.name
   },
@@ -111,64 +165,65 @@ const columns = [
   {
     key: '2',
     title: '货位编码',
-    dataIndex: 'type',
+    dataIndex: 'StorageProduct',
     defaultSortOrder: 'descend',
     sorter: (a, b) => a.age - b.age
   },
   {
     key: '3',
     title: '批次编码',
-    dataIndex: 'unit',
+    dataIndex: 'StorageProduct',
     defaultSortOrder: 'descend',
     sorter: (a, b) => a.age - b.age
   },
   {
     key: '4',
     title: '数量',
-    dataIndex: 'sales_unit_price',
+    dataIndex: 'StorageProduct',
     defaultSortOrder: 'descend',
     sorter: (a, b) => a.age - b.age
   },
   {
     key: '5',
     title: '计量单位',
-    dataIndex: 'purchase_unit_price',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age
-  },
-  {
-    key: '5',
-    title: '包装数量',
-    dataIndex: 'purchase_unit_price',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age
-  },
-  {
-    key: '5',
-    title: '包装单位',
-    dataIndex: 'purchase_unit_price',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age
-  },
-  {
-    key: '5',
-    title: '单价',
-    dataIndex: 'purchase_unit_price',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age
-  },
-  {
-    key: '5',
-    title: '金额',
-    dataIndex: 'purchase_unit_price',
+    dataIndex: 'StorageProduct',
     defaultSortOrder: 'descend',
     sorter: (a, b) => a.age - b.age
   },
   {
     key: '6',
+    title: '包装数量',
+    dataIndex: 'StorageProduct',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age
+  },
+  {
+    key: '7',
+    title: '包装单位',
+    dataIndex: 'StorageProduct',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age
+  },
+  {
+    key: '8',
+    title: '单价',
+    dataIndex: 'StorageProduct',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age
+  },
+  {
+    key: '9',
+    title: '金额',
+    dataIndex: 'StorageProduct',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age
+  },
+  {
+    key: '10',
     title: '操作',
     dataIndex: 'action',
-    width: '150px',
+    width: 120,
+    fixed: 'right',
     scopedSlots: { customRender: 'action' }
   }
 ]
@@ -187,11 +242,16 @@ for (let i = 0; i < 46; i++) {
 const product = {}
 const targetTitle = columns
 export default {
+  components: {
+    STable,
+    STree
+  },
   data() {
     const oriTargetKeys = this.columns
     const targetList = []
     return {
       visible: false,
+      chat_visible: false,
       data,
       product,
       columns,
@@ -201,7 +261,17 @@ export default {
       confirmLoading: false,
       targetKeys: oriTargetKeys,
       selectedKeys: ['0'],
-      disabled: false
+      disabled: false,
+      loadData: parameter => {
+        return getServiceList(Object.assign(parameter, this.queryParam)).then(res => {
+          console.log('/service-->', JSON.stringify(res.result))
+          return res.result
+        })
+      },
+      comments: [],
+      submitting: false,
+      value: '',
+      moment
     }
   },
   computed: {
@@ -277,7 +347,40 @@ export default {
     handleSelectChange(sourceSelectedKeys, targetSelectedKeys) {
       this.selectedKeys = [...sourceSelectedKeys, ...targetSelectedKeys]
     },
-    handleScroll(direction, e) {}
+    handleScroll(direction, e) {},
+    handleSubmit() {
+      if (!this.value) {
+        return
+      }
+
+      this.submitting = true
+
+      setTimeout(() => {
+        this.submitting = false
+        this.comments = [
+          {
+            author: 'Han Solo',
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            content: this.value,
+            datetime: moment().fromNow()
+          },
+          ...this.comments
+        ]
+        this.value = ''
+      }, 1000)
+    },
+    chatChange(e) {
+      this.value = e.target.value
+    },
+    chatClick() {
+      this.chat_visible = true
+    },
+    chatOk(e) {
+      this.chat_visible = false
+    },
+    chatCancel(e) {
+      this.chat_visible = false
+    }
   }
 }
 </script>

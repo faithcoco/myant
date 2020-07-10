1be315c5b83049db9f785d2b40fabe4acecf3934
wuyang0425 <66615157+wuyang0425@users.noreply.github.com>
2020-07-10 21:22:34 +0800

    接口测试

--- src/api/manage.js
+++ src/api/manage.js
@@ -7,12 +7,29 @@ const api = {
   permission: '/permission',
   permissionNoPager: '/permission/no-pager',
   orgTree: '/org/tree',
-  productList:'/productList'
+  productList: '/productList',
+  SupplierList: '/SupplierList',
+  CustomerList: '/CustomerList',
+  ContactList: '/ContactList',
+  StorageManagementList: '/StorageManagementList',
+  OutManagementList: '/OutManagementList',
+  TransferList: '/TransferList',
+  InventoryList: '/InventoryList',
+  PositionAdjustmentList: '/PositionAdjustmentList',
+  ShippingNoticeList: '/ShippingNoticeList',
+  ReceiptNoticeList: '/ReceiptNoticeList',
+  ReturnNoticeClientList: '/ReturnNoticeClientList',
+  ReturnNoticeSupplierList: '/ReturnNoticeSupplierList',
+  BaleList: '/BaleList',
+  WorkTimeReportApplicationList: '/WorkTimeReportApplicationList',
+  PickingApplicationList: '/PickingApplicationList',
+  WarehouseList: '/WarehouseList',
+  OpeningInventoryList: '/OpeningInventoryList'
 }
 
 export default api
 
-export function getUserList (parameter) {
+export function getUserList(parameter) {
   return axios({
     url: api.user,
     method: 'get',
@@ -20,7 +37,7 @@ export function getUserList (parameter) {
   })
 }
 
-export function getRoleList (parameter) {
+export function getRoleList(parameter) {
   return axios({
     url: api.role,
     method: 'get',
@@ -28,14 +45,14 @@ export function getRoleList (parameter) {
   })
 }
 
-export function getServiceList (parameter) {
+export function getServiceList(parameter) {
   return axios({
     url: api.service,
     method: 'get',
     params: parameter
   })
 }
-export function getProductList (parameter) {
+export function getProductList(parameter) {
   console.log('/getProductList req-->', JSON.stringify(parameter))
   return axios({
     url: api.productList,
@@ -43,8 +60,144 @@ export function getProductList (parameter) {
     params: parameter
   })
 }
+export function getSupplierList(parameter) {
+  console.log('/getSupplierList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.SupplierList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getCustomerList(parameter) {
+  console.log('/getCustomerList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.CustomerList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getContactList(parameter) {
+  console.log('/getContactList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.ContactList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getStorageManagementList(parameter) {
+  console.log('/getStorageManagementList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.StorageManagementList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getOutManagementList(parameter) {
+  console.log('/getOutManagementList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.OutManagementList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getTransferList(parameter) {
+  console.log('/getTransferList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.TransferList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getInventoryList(parameter) {
+  console.log('/getInventoryList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.InventoryList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getPositionAdjustmentList(parameter) {
+  console.log('/getPositionAdjustmentList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.PositionAdjustmentList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getShippingNoticeList(parameter) {
+  console.log('/getShippingNoticeList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.ShippingNoticeList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getReceiptNoticeList(parameter) {
+  console.log('/getReceiptNoticeList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.ReceiptNoticeList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getReturnNoticeClientList(parameter) {
+  console.log('/getReturnNoticeClientList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.ReturnNoticeClientList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getReturnNoticeSupplierList(parameter) {
+  console.log('/getReturnNoticeSupplierList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.ReturnNoticeSupplierList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getBaleList(parameter) {
+  console.log('/getBaleList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.BaleList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getWorkTimeReportApplicationList(parameter) {
+  console.log('/getWorkTimeReportApplicationList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.WorkTimeReportApplicationList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getPickingApplicationList(parameter) {
+  console.log('/getPickingApplicationList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.PickingApplicationList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getWarehouseList(parameter) {
+  console.log('/getWarehouseList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.WarehouseList,
+    method: 'get',
+    params: parameter
+  })
+}
+export function getOpeningInventoryList(parameter) {
+  console.log('/getOpeningInventoryList req-->', JSON.stringify(parameter))
+  return axios({
+    url: api.OpeningInventoryList,
+    method: 'get',
+    params: parameter
+  })
+}
 
-export function getPermissions (parameter) {
+export function getPermissions(parameter) {
   return axios({
     url: api.permissionNoPager,
     method: 'get',
@@ -52,7 +205,7 @@ export function getPermissions (parameter) {
   })
 }
 
-export function getOrgTree (parameter) {
+export function getOrgTree(parameter) {
   return axios({
     url: api.orgTree,
     method: 'get',
@@ -62,7 +215,7 @@ export function getOrgTree (parameter) {
 
 // id == 0 add     post
 // id != 0 update  put
-export function saveService (parameter) {
+export function saveService(parameter) {
   return axios({
     url: api.service,
     method: parameter.id === 0 ? 'post' : 'put',

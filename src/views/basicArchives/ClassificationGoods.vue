64aeff2bcc49b9e6c893085c5bd4dbdc02d3ef41
faithcoco <i13166380861@163.com>
2020-07-27 12:45:00 +0800

    功能开发

--- src/views/basicArchives/ClassificationGoods.vue
+++ src/views/basicArchives/ClassificationGoods.vue
@@ -12,14 +12,14 @@
       </a-col>
     </a-row>
     <br />
-    <a-table :columns="columns" :data-source="data" :defaultExpandAllRows="true">
+    <a-table :columns="columns" :data-source="list" defaultExpandAllRows>
       <span slot="action" slot-scope="text, record">
         <template>
           <a @click="handleAddItem(record)">添加子类</a>
           <a-divider type="vertical" />
           <a @click="handleEdit(record)">编辑</a>
           <a-divider type="vertical" />
-          <a-popconfirm v-if="data.length" title="确定要删除吗?" @confirm="() => onDelete(record.key)">
+          <a-popconfirm v-if="list.length" title="确定要删除吗?" @confirm="() => onDelete(record.key)">
             <a href="javascript:;">删除</a>
           </a-popconfirm>
         </template>
@@ -30,32 +30,34 @@
     </a-table>
     <a-modal v-model="visible" title="新增类别" @ok="handleOk">
       <p>请输入类别名称：</p>
-          <a-input ref="userNameInput" v-model="userName" placeholder=""/>
+      <a-input ref="userNameInput" v-model="typeName" placeholder />
     </a-modal>
   </a-card>
 </template>
 <script>
 import { getclassificationGoodsColumns, getclassificationGoodsList } from '@/api/manage'
-const columns = []
-
-const data = []
 
 export default {
   data() {
     return {
-      data,
-      columns,
-      visible:false
+      list: [],
+      columns: [],
+      visible: false,
+      typeName: '',
+      currentRecord: '',
+      tag: -1,
+     
     }
   },
   created() {
-    getclassificationGoodsColumns().then(res => {
+    getclassificationGoodsColumns().then((res) => {
       this.columns = res.columns
       console.log('columns-->', JSON.stringify(res.columns))
     })
-    getclassificationGoodsList().then(res => {
-      this.data = res.result
-        console.log('columns-->', JSON.stringify(res.result))
+    getclassificationGoodsList().then((res) => {
+      this.list = res.result
+      console.log('data-->', JSON.stringify(res.result))
+    
     })
   },
   methods: {
@@ -63,7 +65,9 @@ export default {
       console.log(`checked = ${e.target.checked}`)
     },
     handleAddItem(record) {
-      console.log(record)
+      this.currentRecord = record
+      this.tag = 2
+      this.visible=true
     },
     handleEdit(record) {
       console.log(record)
@@ -72,16 +76,34 @@ export default {
       console.log(record)
     },
     handleAdd() {
+      this.tag = 1
       this.visible = true
     },
     handleOk(e) {
-      console.log(e)
       this.visible = false
+      if (this.tag == 1) {
+        this.list.push({
+          key: this.list.length + 1,
+          name: this.typeName,
+        })
+      } else if (this.tag == 2) {
+        for (const key in this.list) {
+          if (this.list[key] == this.currentRecord) {
+            this.currentRecord.children.push({
+              key: this.list[key].key + '' + (this.currentRecord.children.length + 1),
+              name: this.typeName,
+            })
+            console.log(JSON.stringify(this.currentRecord))
+          }
+        }
+      }
+
+      this.typeName = ''
     },
     onDelete(key) {
-      const data = [...this.data]
-      this.data = data.filter(item => item.key !== key)
-    }
-  }
+      const list = [...this.list]
+      this.list = list.filter((item) => item.key !== key)
+    },
+  },
 }
 </script>
--- src/views/management/Sector.vue
+++ src/views/management/Sector.vue
@@ -1,6 +1,6 @@
 <template>
   <a-card>
-    <a-tree :tree-data="treeData" show-icon :default-expanded-keys="['0-0-1']">
+    <a-tree :tree-data="treeData" show-icon :default-expanded-keys="current">
       <a-icon slot="apartment" type="apartment" :style="{ color: '#1890FF' }" />
       <template slot="custom" slot-scope="{ selected }">
         <a-icon :type="selected ? 'frown' : 'frown-o'" />
@@ -14,19 +14,20 @@ import { Tree } from 'ant-design-vue'
 Vue.use(Tree)
 import { getSector } from '@/api/manage'
 
-const treeData = []
 
 export default {
   data() {
     return {
-      treeData
+      treeData:[],
+      current:[]
     }
   },
   created() {
     getSector().then(res => {
-      console.log('res.result---------->', res.result)
+      console.log('res.result---------->', JSON.stringify(res.result))
       this.treeData = res.result
     })
+    this.current=['0-0-2-0','0-0-2-1']
   },
   methods: {
     onSelect(selectedKeys, info) {

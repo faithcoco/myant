16a8becebd55dfc41d32391aa38947cdeb9dd206
faithcoco <i13166380861@163.com>
2020-08-17 16:48:35 +0800

    设置默认展开

--- src/views/management/Sector.vue
+++ src/views/management/Sector.vue
@@ -1,6 +1,12 @@
 <template>
   <a-card>
-    <a-tree :tree-data="treeData" show-icon :default-expanded-keys="current">
+    <a-tree
+      :tree-data="treeData"
+      show-icon
+      @expand="onExpand"
+      :expanded-keys="expandedKeys"
+      :auto-expand-parent="autoExpandParent"
+    >
       <a-icon slot="apartment" type="apartment" :style="{ color: '#1890FF' }" />
       <template slot="custom" slot-scope="{ selected }">
         <a-icon :type="selected ? 'frown' : 'frown-o'" />
@@ -14,20 +20,20 @@ import { Tree } from 'ant-design-vue'
 Vue.use(Tree)
 import { getSector } from '@/api/manage'
 
-
 export default {
   data() {
     return {
-      treeData:[],
-      current:[]
+      treeData: [],
+      expandedKeys: [],
+      autoExpandParent: true,
     }
   },
   created() {
-    getSector().then(res => {
+    getSector().then((res) => {
       console.log('res.result---------->', JSON.stringify(res.result))
       this.treeData = res.result
+      this.expandedKeys=['0-0-0', '0-0-1']
     })
-    this.current=['0-0-2-0','0-0-2-1']
   },
   methods: {
     onSelect(selectedKeys, info) {
@@ -35,7 +41,13 @@ export default {
     },
     onCheck(checkedKeys, info) {
       console.log('onCheck', checkedKeys, info)
-    }
-  }
+    },
+    onExpand(expandedKeys) {
+      // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+      // or, you can remove all expanded children keys.
+      this.expandedKeys = expandedKeys
+      this.autoExpandParent = false
+    },
+  },
 }
 </script>

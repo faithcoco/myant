87c9ff270e72998018a30ac30d17529524943f7b
wuyang <727226831@qq.com>
2020-08-17 15:18:42 +0800

    路由调整，个人中心改为首页

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -7,9 +7,84 @@ export const asyncRouterMap = [
     path: '/',
     name: 'index',
     component: BasicLayout,
-    meta: { title: '首页' },
+    // meta: { title: '首页' },
     redirect: '/dashboard/workplace',
     children: [
+      {
+        path: '/account',
+        component: RouteView,
+        redirect: '/account/center',
+        name: 'account',
+        meta: { title: '首页', icon: 'user', keepAlive: true, permission: ['user'] },
+        children: [
+          {
+            path: '/dashboard/analysis',
+            name: 'Analysis',
+            component: () => import('@/views/dashboard/Analysis'),
+            meta: { title: '分析页', permission: ['dashboard'] }
+          },
+          {
+            path: '/dashboard/monitor',
+            name: 'Monitor',
+            hidden: true,
+            component: () => import('@/views/dashboard/Monitor'),
+            meta: { title: '监控页', permission: ['dashboard'] }
+          },
+          {
+            path: '/dashboard/workplace',
+            name: 'Workplace',
+            component: RouteView,
+            component: () => import('@/views/dashboard/Workplace'),
+            meta: { title: '工作台', permission: ['dashboard'] }
+          },
+          {
+            path: '/account/center',
+            name: 'center',
+            component: () => import('@/views/account/center/Index'),
+            meta: { title: '个人中心', keepAlive: true, permission: ['user'] }
+          },
+          {
+            path: '/account/settings',
+            name: 'settings',
+            component: () => import('@/views/account/settings/Index'),
+            meta: { title: '个人设置', hideHeader: true, permission: ['user'] },
+            redirect: '/account/settings/base',
+            hideChildrenInMenu: true,
+            children: [
+              {
+                path: '/account/settings/base',
+                name: 'BaseSettings',
+                component: () => import('@/views/account/settings/BaseSetting'),
+                meta: { title: '基本设置', hidden: true, permission: ['user'] }
+              },
+              {
+                path: '/account/settings/security',
+                name: 'SecuritySettings',
+                component: () => import('@/views/account/settings/Security'),
+                meta: { title: '安全设置', hidden: true, keepAlive: true, permission: ['user'] }
+              },
+              {
+                path: '/account/settings/custom',
+                name: 'CustomSettings',
+                component: () => import('@/views/account/settings/Custom'),
+                meta: { title: '个性化设置', hidden: true, keepAlive: true, permission: ['user'] }
+              },
+              {
+                path: '/account/settings/binding',
+                name: 'BindingSettings',
+                component: () => import('@/views/account/settings/Binding'),
+                meta: { title: '账户绑定', hidden: true, keepAlive: true, permission: ['user'] }
+              },
+              {
+                path: '/account/settings/notification',
+                name: 'NotificationSettings',
+                component: () => import('@/views/account/settings/Notification'),
+                meta: { title: '新消息通知', hidden: true, keepAlive: true, permission: ['user'] }
+              }
+            ]
+          }
+        ]
+      },
       {
         path: '/basic_archives',
         name: 'basic_archives',
@@ -406,7 +481,7 @@ export const asyncRouterMap = [
             meta: { title: '收发存汇总表', icon: 'user', permission: ['dashboard'] }
           }
         ]
-      },
+      }
       // {
       //   path: '/panel',
       //   name: '/panel',
@@ -427,144 +502,6 @@ export const asyncRouterMap = [
       //     }
       //   ]
       // },
-
-      {
-        path: '/account',
-        component: RouteView,
-        redirect: '/account/center',
-        name: 'account',
-        meta: { title: '个人中心', icon: 'user', keepAlive: true, permission: ['user'] },
-        children: [
-          // {
-          //   path: '/message',
-          //   name: 'message',
-          //   component: RouteView,
-          //   meta: { title: '消息中心', permission: ['dashboard'] },
-          //   component: () => import(/* webpackChunkName: "fail" */ '@/views/MessageCenter.vue')
-          // },
-          {
-            path: '/dashboard/analysis',
-            name: 'Analysis',
-            component: () => import('@/views/dashboard/Analysis'),
-            meta: { title: '分析页', permission: ['dashboard'] }
-          },
-          {
-            path: '/dashboard/monitor',
-            name: 'Monitor',
-            hidden: true,
-            component: () => import('@/views/dashboard/Monitor'),
-            meta: { title: '监控页', permission: ['dashboard'] }
-          },
-          {
-            path: '/dashboard/workplace',
-            name: 'Workplace',
-            component: RouteView,
-            component: () => import('@/views/dashboard/Workplace'),
-            meta: { title: '工作台', permission: ['dashboard'] }
-          },
-          {
-            path: '/account/center',
-            name: 'center',
-            component: () => import('@/views/account/center/Index'),
-            meta: { title: '个人中心', keepAlive: true, permission: ['user'] }
-          },
-          {
-            path: '/account/settings',
-            name: 'settings',
-            component: () => import('@/views/account/settings/Index'),
-            meta: { title: '个人设置', hideHeader: true, permission: ['user'] },
-            redirect: '/account/settings/base',
-            hideChildrenInMenu: true,
-            children: [
-              {
-                path: '/account/settings/base',
-                name: 'BaseSettings',
-                component: () => import('@/views/account/settings/BaseSetting'),
-                meta: { title: '基本设置', hidden: true, permission: ['user'] }
-              },
-              {
-                path: '/account/settings/security',
-                name: 'SecuritySettings',
-                component: () => import('@/views/account/settings/Security'),
-                meta: { title: '安全设置', hidden: true, keepAlive: true, permission: ['user'] }
-              },
-              {
-                path: '/account/settings/custom',
-                name: 'CustomSettings',
-                component: () => import('@/views/account/settings/Custom'),
-                meta: { title: '个性化设置', hidden: true, keepAlive: true, permission: ['user'] }
-              },
-              {
-                path: '/account/settings/binding',
-                name: 'BindingSettings',
-                component: () => import('@/views/account/settings/Binding'),
-                meta: { title: '账户绑定', hidden: true, keepAlive: true, permission: ['user'] }
-              },
-              {
-                path: '/account/settings/notification',
-                name: 'NotificationSettings',
-                component: () => import('@/views/account/settings/Notification'),
-                meta: { title: '新消息通知', hidden: true, keepAlive: true, permission: ['user'] }
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  },
-  {
-    path: '/account',
-    component: RouteView,
-    redirect: '/account/center',
-    name: 'account',
-    meta: { title: '个人页', icon: 'user', keepAlive: true, permission: ['user'] },
-    children: [
-      {
-        path: '/account/center',
-        name: 'center',
-        component: () => import('@/views/account/center/Index'),
-        meta: { title: '个人中心', keepAlive: true, permission: ['user'] }
-      },
-      {
-        path: '/account/settings',
-        name: 'settings',
-        component: () => import('@/views/account/settings/Index'),
-        meta: { title: '个人设置', hideHeader: true, permission: ['user'] },
-        redirect: '/account/settings/base',
-        hideChildrenInMenu: true,
-        children: [
-          {
-            path: '/account/settings/base',
-            name: 'BaseSettings',
-            component: () => import('@/views/account/settings/BaseSetting'),
-            meta: { title: '基本设置', hidden: true, permission: ['user'] }
-          },
-          {
-            path: '/account/settings/security',
-            name: 'SecuritySettings',
-            component: () => import('@/views/account/settings/Security'),
-            meta: { title: '安全设置', hidden: true, keepAlive: true, permission: ['user'] }
-          },
-          {
-            path: '/account/settings/custom',
-            name: 'CustomSettings',
-            component: () => import('@/views/account/settings/Custom'),
-            meta: { title: '个性化设置', hidden: true, keepAlive: true, permission: ['user'] }
-          },
-          {
-            path: '/account/settings/binding',
-            name: 'BindingSettings',
-            component: () => import('@/views/account/settings/Binding'),
-            meta: { title: '账户绑定', hidden: true, keepAlive: true, permission: ['user'] }
-          },
-          {
-            path: '/account/settings/notification',
-            name: 'NotificationSettings',
-            component: () => import('@/views/account/settings/Notification'),
-            meta: { title: '新消息通知', hidden: true, keepAlive: true, permission: ['user'] }
-          }
-        ]
-      }
     ]
   },
   {
--- src/views/dashboard/Workplace.vue
+++ src/views/dashboard/Workplace.vue
@@ -23,11 +23,76 @@
 
     <div>
       <a-row :gutter="24">
-        <a-col :xl="16" :lg="24" :md="24" :sm="24" :xs="24">
+        <a-col :xl="24" :lg="24" :md="24" :sm="24" :xs="24">
+          <a-card :loading="loading" title="审批" :bordered="false">
+            <a-radio-group v-model="size" style="margin-bottom: 16px" @change="onChange">
+              <a-radio-button value="approval">审批待办</a-radio-button>
+              <a-radio-button value="notification">系统通知</a-radio-button>
+              <a-radio-button value="contract">合同动态</a-radio-button>
+            </a-radio-group>
+            <!-- <div v-show="isShow">
+              <a-tabs default-active-key="1" :size="size" @change="callback">
+                <a-tab-pane key="1" tab="待我处理"></a-tab-pane>
+                <a-tab-pane key="2" tab="我已处理"></a-tab-pane>
+                <a-tab-pane key="3" tab="我发起的"></a-tab-pane>
+                <a-tab-pane key="4" tab="抄送我的"></a-tab-pane>
+              </a-tabs>
+            </div>-->
+
+            <div class="group1">
+              <a-input-search class="search" placeholder="请输入搜索内容" style="width: 200px" @search="onSearch" />
+              <br />
+              <br />
+              <a-radio-group class="radio1" default-value="a" button-style="solid">
+                {{ type }}
+                <a-radio-button value="a">全部</a-radio-button>
+                <a-radio-button value="b">@我的</a-radio-button>
+                <a-radio-button value="c">非@我的</a-radio-button>
+              </a-radio-group>
+              <br />
+              <br />
+
+              <div class="group2">
+                <div class="group2-btn1">
+                  <a-radio-group default-value="a" button-style="solid">
+                    {{ module }}
+                    <a-radio-button value="a">全部</a-radio-button>
+                    <a-radio-button value="b">应收</a-radio-button>
+                    <a-radio-button value="c">应付</a-radio-button>
+                  </a-radio-group>
+
+                  <a-radio class="read">仅显示未读</a-radio>
+                </div>
+                <a-radio-group class="group2-btn2" default-value="a" button-style="solid">
+                  <a-radio-button value="a">标为已读</a-radio-button>
+                  <a-radio-button value="b">导出</a-radio-button>
+                </a-radio-group>
+              </div>
+
+              <a-table :columns="columns" :data-source="data" style="margin-top:20px" bordered>
+                <a slot="name" slot-scope="text">{{ text }}</a>
+              </a-table>
+            </div>
+            <!-- <a-list>
+              <a-list-item :key="index" v-for="(item, index) in activities">
+                <a-list-item-meta>
+                  <a-avatar slot="avatar" :src="item.user.avatar" />
+                  <div slot="title">
+                    <span>{{ item.user.nickname }}</span>&nbsp;
+                    在&nbsp;
+                    <a href="#">{{ item.project.name }}</a>&nbsp;
+                    <span>{{ item.project.action }}</span>&nbsp;
+                    <a href="#">{{ item.project.event }}</a>
+                  </div>
+                  <div slot="description">{{ item.time }}</div>
+                </a-list-item-meta>
+              </a-list-item>
+            </a-list>-->
+          </a-card>
           <a-card
             class="project-list"
             :loading="loading"
-            style="margin-bottom: 24px;"
+            style="margin-top: 24px;"
             :bordered="false"
             title="进行中的项目"
             :body-style="{ padding: 0 }"
@@ -90,12 +155,7 @@
                       <a-icon type="info-circle-o" />
                     </a-tooltip>
                     <div>
-                      <mini-progress
-                        color="rgb(19, 194, 194)"
-                        :target="80"
-                        :percentage="78"
-                        height="8px"
-                      />
+                      <mini-progress color="rgb(19, 194, 194)" :target="80" :percentage="78" height="8px" />
                     </div>
                     <template slot="footer">
                       <trend flag="down" style="margin-right: 16px;">
@@ -108,119 +168,6 @@
                       </trend>
                     </template>
                   </chart-card>
-                </a-col>
-              </a-row>
-            </div>
-          </a-card>
-
-          <a-card :loading="loading" title="审批" :bordered="false">
-            <a-radio-group v-model="size" style="margin-bottom: 16px" @change="onChange">
-              <a-radio-button value="approval">审批待办</a-radio-button>
-              <a-radio-button value="notification">系统通知</a-radio-button>
-              <a-radio-button value="contract">合同动态</a-radio-button>
-            </a-radio-group>
-            <!-- <div v-show="isShow">
-              <a-tabs default-active-key="1" :size="size" @change="callback">
-                <a-tab-pane key="1" tab="待我处理"></a-tab-pane>
-                <a-tab-pane key="2" tab="我已处理"></a-tab-pane>
-                <a-tab-pane key="3" tab="我发起的"></a-tab-pane>
-                <a-tab-pane key="4" tab="抄送我的"></a-tab-pane>
-              </a-tabs>
-            </div>-->
-
-            <div class="group1">
-              <a-input-search
-                class="search"
-                placeholder="请输入搜索内容"
-                style="width: 200px"
-                @search="onSearch"
-              />
-              <br />
-              <br />
-              <a-radio-group class="radio1" default-value="a" button-style="solid">
-                {{type}}
-                <a-radio-button value="a">全部</a-radio-button>
-                <a-radio-button value="b">@我的</a-radio-button>
-                <a-radio-button value="c">非@我的</a-radio-button>
-              </a-radio-group>
-              <br />
-              <br />
-
-              <div class="group2">
-                <div class="group2-btn1">
-                  <a-radio-group default-value="a" button-style="solid">
-                    {{module}}
-                    <a-radio-button value="a">全部</a-radio-button>
-                    <a-radio-button value="b">应收</a-radio-button>
-                    <a-radio-button value="c">应付</a-radio-button>
-                  </a-radio-group>
-
-                  <a-radio class="read">仅显示未读</a-radio>
-                </div>
-                <a-radio-group class="group2-btn2" default-value="a" button-style="solid">
-                  <a-radio-button value="a">标为已读</a-radio-button>
-                  <a-radio-button value="b">导出</a-radio-button>
-                </a-radio-group>
-              </div>
-
-              <a-table :columns="columns" :data-source="data" style="margin-top:20px" bordered>
-                <a slot="name" slot-scope="text">{{ text }}</a>
-              </a-table>
-            </div>
-            <!-- <a-list>
-              <a-list-item :key="index" v-for="(item, index) in activities">
-                <a-list-item-meta>
-                  <a-avatar slot="avatar" :src="item.user.avatar" />
-                  <div slot="title">
-                    <span>{{ item.user.nickname }}</span>&nbsp;
-                    在&nbsp;
-                    <a href="#">{{ item.project.name }}</a>&nbsp;
-                    <span>{{ item.project.action }}</span>&nbsp;
-                    <a href="#">{{ item.project.event }}</a>
-                  </div>
-                  <div slot="description">{{ item.time }}</div>
-                </a-list-item-meta>
-              </a-list-item>
-            </a-list>-->
-          </a-card>
-        </a-col>
-        <a-col style="padding: 0 12px" :xl="8" :lg="24" :md="24" :sm="24" :xs="24">
-          <a-card
-            title="快速开始 / 便捷导航"
-            style="margin-bottom: 24px"
-            :bordered="false"
-            :body-style="{padding: 0}"
-          >
-            <div class="item-group">
-              <a>操作一</a>
-              <a>操作二</a>
-              <a>操作三</a>
-              <a>操作四</a>
-              <a>操作五</a>
-              <a>操作六</a>
-              <a-button size="small" type="primary" ghost icon="plus">添加</a-button>
-            </div>
-          </a-card>
-          <a-card
-            title="XX 指数"
-            style="margin-bottom: 24px"
-            :loading="radarLoading"
-            :bordered="false"
-            :body-style="{ padding: 0 }"
-          >
-            <div style="min-height: 400px;">
-              <!-- :scale="scale" :axis1Opts="axis1Opts" :axis2Opts="axis2Opts"  -->
-              <radar :data="radarData" />
-            </div>
-          </a-card>
-          <a-card :loading="loading" title="团队" :bordered="false">
-            <div class="members">
-              <a-row>
-                <a-col :span="12" v-for="(item, index) in teams" :key="index">
-                  <a>
-                    <a-avatar size="small" :src="item.avatar" />
-                    <span class="member">{{ item.name }}</span>
-                  </a>
                 </a-col>
               </a-row>
             </div>
@@ -247,7 +194,7 @@ import {
   Bar,
   Trend,
   NumberInfo,
-  MiniSmoothArea,
+  MiniSmoothArea
 } from '@/components'
 
 import { getRoleList, getServiceList } from '@/api/manage'
@@ -259,33 +206,33 @@ const columns = [
     title: '标题',
     width: 200,
     dataIndex: 'title',
-    key: 'title',
+    key: 'title'
   },
   {
     title: '内容',
     width: 200,
     dataIndex: 'LR',
-    key: 'LR',
+    key: 'LR'
   },
   {
     title: '来源/合同名称',
     width: 200,
     dataIndex: 'name',
-    key: 'name',
+    key: 'name'
   },
   {
     title: '时间',
     width: 100,
     dataIndex: 'time',
     key: 'time',
-    sorter: (a, b) => a.age - b.age,
+    sorter: (a, b) => a.age - b.age
   },
   {
     title: '状态',
     width: 60,
     dataIndex: 'status',
-    key: 'status',
-  },
+    key: 'status'
+  }
 ]
 
 const data = [
@@ -295,8 +242,8 @@ const data = [
     LR: '@高铭亮，高总，我的项目什么时候审批好',
     name: '上海固圣信息技术有限公司一次性服务款项目',
     time: '2020-04-21',
-    status: '未读',
-  },
+    status: '未读'
+  }
 ]
 
 export default {
@@ -313,7 +260,7 @@ export default {
     Bar,
     Trend,
     NumberInfo,
-    MiniSmoothArea,
+    MiniSmoothArea
   },
   data() {
     return {
@@ -342,10 +289,10 @@ export default {
         tickLine: null,
         grid: {
           lineStyle: {
-            lineDash: null,
+            lineDash: null
           },
-          hideFirstLine: false,
-        },
+          hideFirstLine: false
+        }
       },
       axis2Opts: {
         dataKey: 'score',
@@ -354,16 +301,16 @@ export default {
         grid: {
           type: 'polygon',
           lineStyle: {
-            lineDash: null,
-          },
-        },
+            lineDash: null
+          }
+        }
       },
       scale: [
         {
           dataKey: 'score',
           min: 0,
-          max: 80,
-        },
+          max: 80
+        }
       ],
       axisData: [
         { item: '引用', a: 70, b: 30, c: 40 },
@@ -371,29 +318,29 @@ export default {
         { item: '产量', a: 50, b: 60, c: 40 },
         { item: '贡献', a: 40, b: 50, c: 40 },
         { item: '热度', a: 60, b: 70, c: 40 },
-        { item: '引用', a: 70, b: 50, c: 40 },
+        { item: '引用', a: 70, b: 50, c: 40 }
       ],
-      radarData: [],
+      radarData: []
     }
   },
   computed: {
     ...mapState({
-      nickname: (state) => state.user.nickname,
-      welcome: (state) => state.user.welcome,
+      nickname: state => state.user.nickname,
+      welcome: state => state.user.welcome
     }),
     userInfo() {
       return this.$store.getters.userInfo
-    },
+    }
   },
   created() {
     this.user = this.userInfo
     this.avatar = this.userInfo.avatar
 
-    getRoleList().then((res) => {
+    getRoleList().then(res => {
       // console.log('workplace -> call getRoleList()', res)
     })
 
-    getServiceList().then((res) => {
+    getServiceList().then(res => {
       // console.log('workplace -> call getServiceList()', res)
     })
   },
@@ -405,20 +352,20 @@ export default {
   },
   methods: {
     getProjects() {
-      this.$http.get('/list/search/projects').then((res) => {
+      this.$http.get('/list/search/projects').then(res => {
         console.log('/list/search/projects res-->', JSON.stringify(res))
         this.projects = res.result && res.result.data
         this.loading = false
       })
     },
     getActivity() {
-      this.$http.get('/workplace/activity').then((res) => {
+      this.$http.get('/workplace/activity').then(res => {
         console.log('/workplace/activity res-->', JSON.stringify(res))
         this.activities = res.result
       })
     },
     getTeams() {
-      this.$http.get('/workplace/teams').then((res) => {
+      this.$http.get('/workplace/teams').then(res => {
         console.log('/workplace/teams res-->', JSON.stringify(res))
         this.teams = res.result
       })
@@ -426,14 +373,14 @@ export default {
     initRadar() {
       this.radarLoading = true
 
-      this.$http.get('/workplace/radar').then((res) => {
+      this.$http.get('/workplace/radar').then(res => {
         console.log('/workplace/radar res-->', JSON.stringify(res))
         const dv = new DataSet.View().source(res.result)
         dv.transform({
           type: 'fold',
           fields: ['个人', '团队', '部门'],
           key: 'user',
-          value: 'score',
+          value: 'score'
         })
 
         this.radarData = dv.rows
@@ -462,8 +409,8 @@ export default {
     },
     callback(value) {
       console.log(value)
-    },
-  },
+    }
+  }
 }
 </script>
 

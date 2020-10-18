d4b4ce82ce81f3e919fed80160e693cb32b4d32b
YS <18817980832@163.com>
2020-10-18 09:38:19 +0800

    ys

--- src/views/management/PersonnelSetting.vue
+++ src/views/management/PersonnelSetting.vue
@@ -1,6 +1,18 @@
 <template>
   <a-card :bordered="false">
     <a-row :gutter="8">
+      <a-col :span="8">
+      </a-col>
+      <a-col :span="16">
+          <span class="table-page-search-submitButtons" :style="{ float: 'right', overflow: 'hidden' } || {}">
+            <a-button style="margin-left: 5px" type="primary" @click="add()">新增</a-button>
+            <a-button style="margin-left: 5px" type="primary" @click="handleSetting()">设置</a-button>
+            <a-button style="margin-left: 5px" @click="() => (queryParam = {})">导入</a-button>
+            <a-button style="margin-left: 5px" @click="() => (queryParam = {})">导出</a-button>
+          </span>
+      </a-col>
+    </a-row>
+    <a-row :gutter="8">
       <a-col :span="5">
         <span>部门结构</span>
         <a-tree
@@ -23,35 +35,34 @@
           :columns="columns"
           :data="loadData"
           :alert="false"
-           :scroll="{ x: 1300 }"
+          :scroll="{ x: 1300 }"
           :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
         >
           <span slot="action" slot-scope="text, record">
-            <template v-if="$auth('table.update')">
+            <template>
               <a @click="handleEdit(record)">编辑</a>
               <a-divider type="vertical" />
             </template>
             <a-dropdown>
               <a class="ant-dropdown-link">
-                更多
-                <a-icon type="down" />
+                更多<a-icon type="down" />
               </a>
               <a-menu slot="overlay">
                 <a-menu-item>
                   <a href="javascript:;">详情</a>
                 </a-menu-item>
-                <a-menu-item v-if="$auth('table.disable')">
+                <a-menu-item >
                   <a href="javascript:;">禁用</a>
                 </a-menu-item>
-                <a-menu-item v-if="$auth('table.delete')">
+                <a-menu-item >
                   <a href="javascript:;">删除</a>
                 </a-menu-item>
               </a-menu>
             </a-dropdown>
           </span>
         </s-table>
-        
       </a-col>
+
     </a-row>
 
     <org-modal ref="modal" @ok="handleSaveOk" @close="handleSaveClose" />
@@ -59,143 +70,153 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import STree from '@/components/Tree/Tree'
-import { STable } from '@/components'
-import { Tree } from 'ant-design-vue'
-Vue.use(Tree)
-import OrgModal from '../other/modules/OrgModal'
-import { getOrgTree, getServiceList, getPersonnelSettingList, getPersonnelSettingColumns} from '@/api/manage'
-import { getSector } from '@/api/manage'
-import { logininfo } from '@/store/mutation-types'
+  import Vue from 'vue'
+  import STree from '@/components/Tree/Tree'
+  import { STable } from '@/components'
+  import { Tree } from 'ant-design-vue'
+  Vue.use(Tree)
+  import OrgModal from '../other/modules/OrgModal'
+  import { getOrgTree, getServiceList, getPersonnelSettingList, getPersonnelSettingColumns} from '@/api/manage'
+  import { getSector } from '@/api/manage'
+  import { logininfo } from '@/store/mutation-types'
 
-const columns = []
+  const columns = []
 
-export default {
-  name: 'TreeList',
-  components: {
-    STable,
-    STree,
-    OrgModal,
-  },
-  data() {
-    return {
-      openKeys: ['key-01'],
-      treeData: [],
-      expandedKeys: [],
-      autoExpandParent: true,
-      // 查询参数
-      queryParam: {},
-      // 表头
-      columns,
-      // 加载数据方法 必须为 Promise 对象
-      loadData: (parameter) => {
-        parameter.menuid='03bf0fb1-e9fb-4014-92e7-7121f4f71005'
-        parameter.enterpriseid = Vue.ls.get(logininfo).basepersonPO.enterpriseid
-        return getPersonnelSettingList(Object.assign(parameter, this.queryParam)).then((res) => {
-          console.log("getPersonnelSettingList-->",JSON.stringify(res))
-          return res.result
-        })
-      },
-      orgTree: [],
-      selectedRowKeys: [],
-      selectedRows: [],
-    }
-  },
-  created() {
-    const columnsParams = {}
-    columnsParams.menuid = '03bf0fb1-e9fb-4014-92e7-7121f4f71002'
-    columnsParams.enterpriseid = Vue.ls.get(logininfo).basepersonPO.enterpriseid
-    getPersonnelSettingColumns(columnsParams).then((res) => {
-      console.log("getPersonnelSettingColumns-->",JSON.stringify(res))
-      this.columns = res.result.columns
-    })
-    const treeParams = {}
-    treeParams.enterpriseid = Vue.ls.get(logininfo).basepersonPO.enterpriseid
-    getSector(treeParams).then((res) => {
-       console.log("getSector-->",JSON.stringify(res))
-      this.treeData = res.result
-      this.expandedKeys = ['0-0-0', '0-0-1']
-    })
-    getOrgTree().then((res) => {
-      this.orgTree = res.result
-    })
-  },
-  methods: {
-    onSelect(selectedKeys, info) {
-      console.log('selected', selectedKeys, info)
+  export default {
+    name: 'TreeList',
+    components: {
+      STable,
+      STree,
+      OrgModal,
     },
-    onCheck(checkedKeys, info) {
-      console.log('onCheck', checkedKeys, info)
-    },
-    onExpand(expandedKeys) {
-      // if not set autoExpandParent to false, if children expanded, parent can not collapse.
-      // or, you can remove all expanded children keys.
-      this.expandedKeys = expandedKeys
-      this.autoExpandParent = false
-    },
-    handleClick(e) {
-      console.log('handleClick', e)
-      this.queryParam = {
-        key: e.key,
+    data() {
+      return {
+        openKeys: ['key-01'],
+        treeData: [],
+        expandedKeys: [],
+        autoExpandParent: true,
+        // 查询参数
+        queryParam: {},
+        // 表头
+        columns,
+        // 加载数据方法 必须为 Promise 对象
+        loadData: (parameter) => {
+          parameter.menuid='03bf0fb1-e9fb-4014-92e7-7121f4f71005'
+          parameter.enterpriseid = Vue.ls.get(logininfo).basepersonPO.enterpriseid
+          return getPersonnelSettingList(Object.assign(parameter, this.queryParam)).then((res) => {
+            console.log("getPersonnelSettingList-->",JSON.stringify(res))
+            return res.result
+          })
+        },
+        orgTree: [],
+        selectedRowKeys: [],
+        selectedRows: [],
       }
-      this.$refs.table.refresh(true)
     },
-    handleAdd(item) {
-      console.log('add button, item', item)
-      this.$message.info(`提示：你点了 ${item.key} - ${item.title} `)
-      this.$refs.modal.add(item.key)
+    created() {
+      const columnsParams = {}
+      columnsParams.menuid = '03bf0fb1-e9fb-4014-92e7-7121f4f71002'
+      columnsParams.enterpriseid = Vue.ls.get(logininfo).basepersonPO.enterpriseid
+      getPersonnelSettingColumns(columnsParams).then((res) => {
+        console.log("getPersonnelSettingColumns-->",JSON.stringify(res))
+        this.columns = res.result.columns
+      })
+      const treeParams = {}
+      treeParams.enterpriseid = Vue.ls.get(logininfo).basepersonPO.enterpriseid
+      getSector(treeParams).then((res) => {
+        console.log("getSector-->",JSON.stringify(res))
+        this.treeData = res.result
+        this.expandedKeys = ['0-0-0', '0-0-1']
+      })
+      getOrgTree().then((res) => {
+        this.orgTree = res.result
+      })
     },
-    handleTitleClick(item) {
-      console.log('handleTitleClick', item)
-    },
-    titleClick(e) {
-      console.log('titleClick', e)
-    },
-    handleSaveOk() {},
-    handleSaveClose() {},
+    methods: {
+      onSelect(selectedKeys, info) {
+        console.log('selected', selectedKeys, info)
+      },
+      onCheck(checkedKeys, info) {
+        console.log('onCheck', checkedKeys, info)
+      },
+      onExpand(expandedKeys) {
+        // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+        // or, you can remove all expanded children keys.
+        this.expandedKeys = expandedKeys
+        this.autoExpandParent = false
+      },
+      handleClick(e) {
+        console.log('handleClick', e)
+        this.queryParam = {
+          key: e.key,
+        }
+        this.$refs.table.refresh(true)
+      },
+      handleAdd(item) {
+        console.log('add button, item', item)
+        this.$message.info(`提示：你点了 ${item.key} - ${item.title} `)
+        this.$refs.modal.add(item.key)
+      },
+      handleTitleClick(item) {
+        console.log('handleTitleClick', item)
+      },
+      titleClick(e) {
+        console.log('titleClick', e)
+      },
+      handleSaveOk() {},
+      handleSaveClose() {},
 
-    onSelectChange(selectedRowKeys, selectedRows) {
-      this.selectedRowKeys = selectedRowKeys
-      this.selectedRows = selectedRows
+      onSelectChange(selectedRowKeys, selectedRows) {
+        this.selectedRowKeys = selectedRowKeys
+        this.selectedRows = selectedRows
+      },
+      /**
+       * 编辑事件
+       * @param
+       * @author YangNick
+       * @date 2020年10月15日16:09:27
+       */
+      handleEdit(row){
+        debugger;
+        console.log(row);
+      },
     },
-  },
-}
+  }
 </script>
 
 <style lang="less">
-.custom-tree {
-  /deep/ .ant-menu-item-group-title {
-    position: relative;
-    &:hover {
-      .btn {
-        display: block;
+  .custom-tree {
+    /deep/ .ant-menu-item-group-title {
+      position: relative;
+      &:hover {
+        .btn {
+          display: block;
+        }
+      }
+    }
+
+    /deep/ .ant-menu-item {
+      &:hover {
+        .btn {
+          display: block;
+        }
+      }
+    }
+
+    /deep/ .btn {
+      display: none;
+      position: absolute;
+      top: 0;
+      right: 10px;
+      width: 20px;
+      height: 40px;
+      line-height: 40px;
+      z-index: 1050;
+
+      &:hover {
+        transform: scale(1.2);
+        transition: 0.5s all;
       }
     }
   }
-
-  /deep/ .ant-menu-item {
-    &:hover {
-      .btn {
-        display: block;
-      }
-    }
-  }
-
-  /deep/ .btn {
-    display: none;
-    position: absolute;
-    top: 0;
-    right: 10px;
-    width: 20px;
-    height: 40px;
-    line-height: 40px;
-    z-index: 1050;
-
-    &:hover {
-      transform: scale(1.2);
-      transition: 0.5s all;
-    }
-  }
-}
 </style>

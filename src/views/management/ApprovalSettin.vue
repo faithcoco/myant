cbf24395f185d5667a6218424c4f73372fec40d1
faithcoco <i13166380861@163.com>
2020-10-27 18:24:19 +0800

    审批开发

--- src/views/management/ApprovalSettin.vue
+++ src/views/management/ApprovalSettin.vue
@@ -2,6 +2,7 @@
   <a-card :bordered="false">
     <a-row :gutter="8">
       <a-col :span="5">
+          <div class="approvalTree">
         <s-tree
           :dataSource="approvalSettinTree"
           :openKeys.sync="openKeys"
@@ -9,7 +10,10 @@
           @click="handleClick"
           @add="handleAdd"
           @titleClick="handleTitleClick"
+          v-model="selectedKeys"
+           :selectedKeys='selectedKeys'
         ></s-tree>
+          </div>
       </a-col>
       <a-col :span="19">
         <a-form layout="vertical">
@@ -22,29 +26,44 @@
           >
 
           <a-form-item label="审批节点一:">
-            <a-select mode="multiple" style="width: 50%" placeholder="请选择审批人员" @change="handleChange1">
-              <a-select-option v-for="(item, i) in nameList" :key="item.name">
+             <a-select   style="width: 50%"  placeholder="请选择审批人员" @change="handleChange1" v-model="select1"  label-in-value>
+              <a-select-option v-for="(item, i) in nameList" :key="item.key">
                 {{ item.name }}
               </a-select-option>
             </a-select>
           </a-form-item>
-                    <a-form-item label="审批节点二:">
-            <a-select mode="multiple" style="width: 50%" placeholder="请选择审批人员" @change="handleChange2">
-              <a-select-option v-for="(item, i) in nameList" :key="item.name">
+          <a-form-item label="审批节点二:">
+            <a-select   style="width: 50%"  label-in-value placeholder="请选择审批人员"   v-model="select2" @change="handleChange2" > 
+              <a-select-option v-for="(item, i) in nameList" :key="item.key">
                 {{ item.name }}
               </a-select-option>
             </a-select>
           </a-form-item>
-                    <a-form-item label="审批节点三:">
-            <a-select mode="multiple" style="width: 50%" placeholder="请选择审批人员" @change="handleChange3">
-              <a-select-option v-for="(item, i) in nameList" :key="item.name">
+          <a-form-item label="审批节点三:">
+            <a-select   style="width: 50%"  label-in-value placeholder="请选择审批人员"  v-model="select3" @change="handleChange3">
+              <a-select-option v-for="(item, i) in nameList" :key="item.key">
                 {{ item.name }}
               </a-select-option>
             </a-select>
+
           </a-form-item>
           <a-form-item label :required="false">
             <a-steps direction="vertical" size="small" :current="3" progress-dot>
-              <a-step v-for="(item, i) in list" :title="item.title" :description="item.description" />
+              <a-step v-for="(item, i) in list" :title="item.title" :description="item.name" />
+            </a-steps>
+          </a-form-item>
+                       <a-form-item >
+       <a-row>
+         
+          <a-col :span="1" :offset="1">
+            <a-button type="primary" @click="onSubmit">保存</a-button>
+          </a-col>
+          <a-col :span="1" :offset="1">
+            <a-button type @click="onBack">取消</a-button>
+          </a-col>
+        </a-row>
+          </a-form-item>
+        
             </a-steps>
           </a-form-item>
         </a-form>
@@ -59,7 +78,9 @@
 import STree from '@/components/Tree/Tree'
 import { STable } from '@/components'
 import OrgModal from '../other/modules/OrgModal'
-import { getapprovalSettinColumns, getapprovalSettinTree } from '@/api/manage'
+import { getData, postData } from '@/api/manage'
+import { logininfo } from '@/store/mutation-types'
+import Vue from 'vue'
 const columns = []
 export default {
   name: 'TreeList',
@@ -70,12 +91,9 @@ export default {
   },
   data() {
     return {
-      openKeys: ['key-01'],
-      list: [
-        { title: '审批节点一', description: '张三' },
-        { title: '审批节点二', description: '张三' },
-        { title: '审批节点三', description: '张三' },
-      ],
+      openKeys: ['03bf0fb1-e9fb-4014-92e7-7121f4f71002'],
+      selectedKeys: ['03bf0fb1-e9fb-4014-92e7-7121f4f71002'],
+      list: [],
       nameList: [
         { key: 1, name: '张三 ' },
         { key: 2, name: '李四 ' },
@@ -90,36 +108,84 @@ export default {
       approvalSettinTree: [],
       selectedRowKeys: [],
       selectedRows: [],
+      menuid: '',
+      select1: { key: '' },
+      select2: { key: '' },
+      select3: { key: '' },
     }
   },
   created() {
-    getapprovalSettinTree().then((res) => {
-      console.log(res.result)
+    //params.memuname=''
+    var url = '/work/getBillTypeList'
+    const params = {}
+    getData(params, url).then((res) => {
       this.approvalSettinTree = res.result
+      console.log("tree-->",JSON.stringify(this.approvalSettinTree))
+      this.menuid=this.approvalSettinTree[0].memuid
+      this.getSelect()
     })
-    getapprovalSettinColumns().then((res) => {
-      console.log(res.columns)
-      this.columns = res.columns
+    url = '/work/getPsndocOrRoleList'
+    const paramsName = { enterpriseid: Vue.ls.get(logininfo).basepersonPO.enterpriseid }
+    getData(paramsName, url).then((res) => {
+      console.log('name list ->', JSON.stringify(res))
+      this.nameList = res.result.nameList
     })
   },
   methods: {
+    getSelect() {
+      const parameter = {
+        enterpriseid: Vue.ls.get(logininfo).basepersonPO.enterpriseid,
+        memuid: this.menuid,
+      }
+      var url = '/work/getWorkFlowSetInfo'
+      getData(parameter, url).then((res) => {
+        console.log('approval init ->', JSON.stringify(res))
+        this.list = res.result.list
+        this.select1.key = this.list[0].key
+        this.select2.key = this.list[1].key
+        this.select3.key = this.list[2].key
+      })
+    },
+    onSubmit(e) {
+      const parameter = {
+        enterpriseid: Vue.ls.get(logininfo).basepersonPO.enterpriseid,
+        memuid: this.menuid,
+        nameList: this.list,
+      }
+      var url = '/work/insertWorkflow'
+      postData(parameter, url).then((res) => {
+        console.log('name ->', JSON.stringify(res))
+
+        if (res.status == 'SUCCESS') {
+          this.$message.info(res.status)
+        } else {
+          this.$message.error(res.status)
+        }
+      })
+    },
+
+    onBack(e) {
+      this.getSelect()
+    },
     handleChange1(value) {
-      this.list[0].description = value.join(',')
+      console.log(JSON.stringify(value))
+      this.list[0].name = value.label
+      this.list[0].key = value.key
     },
-      handleChange2(value) {
-      this.list[1].description = value.join(',')
+    handleChange2(value) {
+      this.list[1].name = value.label
+      this.list[1].key = value.key
     },
-      handleChange3(value) {
-      this.list[2].description = value.join(',')
+    handleChange3(value) {
+      this.list[2].name = value.label
+      this.list[2].key = value.key
     },
     onChange(checked) {
       console.log(`a-switch to ${checked}`)
     },
     handleClick(e) {
-      console.log('handleClick', e)
-      this.queryParam = {
-        key: e.key,
-      }
+      this.menuid = e.key
+      this.getSelect()
     },
     handleAdd(item) {
       console.log('add button, item', item)
@@ -144,6 +210,13 @@ export default {
 </script>
 
 <style lang="less">
+.approvalTree {
+  border: 1px solid #e8e8e8;
+  border-radius: 4px;
+  overflow: auto;
+  padding: 8px 2px;
+  height: 775px;
+}
 .custom-tree {
   /deep/ .ant-menu-item-group-title {
     position: relative;
